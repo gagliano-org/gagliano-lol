5ed45a4e63c6a9e3057d76bee668b645375094b0
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-23 18:34:07 +0200

    rudimintale gestione dei bloc

--- src/handlers/handleBlogs.js
+++ src/handlers/handleBlogs.js
@@ -1,5 +1,13 @@
+import { readdirSync } from "fs";
 export const handleBlogs = () => {
-  const files = readdirSync("./assets/").map((e) => e.replace(".md", ""));
+  // const files = readdirSync("./assets/").map((e) => e.replace(".md", ""));
+  const giacomoBlogs = readdirSync("./assets/giacomo/").map((e) =>
+    e.replace(".md", "")
+  );
+  const ariannaBlogs = readdirSync("./assets/ari/").map((e) =>
+    e.replace(".md", "")
+  );
+  const files = { giacomo: giacomoBlogs, arianna: ariannaBlogs };
   const headers = {
     "Content-Type": "application/json", // Imposta il tipo di contenuto a HTML
   };
