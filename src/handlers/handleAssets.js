c759d89b0bd6da306ae47f76740ac644894a9811
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-23 18:33:44 +0200

    importa e usa frontMatterInMdToJSObject

--- src/handlers/handleAssets.js
+++ src/handlers/handleAssets.js
@@ -1,8 +1,10 @@
+import { frontMatterInMdToJSObject } from "@zaionstate/server";
 export const handleAssets = async (url) => {
   const filename = url.pathname.replace("/assets/", "");
-  const md = await Bun.file(`./assets/${filename}`).text();
+  const md = await Bun.file(`./assets/${filename}.md`).text();
+  const string = await frontMatterInMdToJSObject(md);
   const headers = {
     "Content-Type": "text/markdown", // Imposta il tipo di contenuto a HTML
   };
-  return new Response(md, { headers });
+  return new Response(JSON.parse(string).value, { headers });
 };
