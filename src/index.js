5ba06c545ed4aa871eef00d336e8c721f08029bd
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-10-23 18:34:31 +0200

    cancella old import di mdToHtmlString

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@
 console.log("gagliano kick asses");
 console.log("bumbalacaca");
 
-import { Pager } from "@zaionstate/ui";
-import { mdToHtmlString } from "./mdToHtmlString";
+import { Pager, UIDesign } from "@zaionstate/ui";
 import page from "page";
 
 const pager = new Pager({ parser: new DOMParser() });
@@ -19,6 +18,7 @@ const indexhtml = `
   <a href="./noa">Noa Josephine</a>
   <a href="./mia">Mia Jacqueline</a>
   <a href="./era">Era Jasmine</a>
+  <a href="./blog">Blogs</a>
 </div>
 `;
 
@@ -52,6 +52,10 @@ const miahtml = ``;
 
 const erahtml = ``;
 
+const blogHtml = `
+<div id="container"></div>
+`;
+
 const index = pager.callbackMaker(indexhtml);
 
 const giacomo = pager.callbackMaker(giacomohtml);
@@ -64,10 +68,47 @@ const mia = pager.callbackMaker(miahtml);
 
 const era = pager.callbackMaker(erahtml);
 
-function blog() {
-  const res = fetch("/blogs");
-  console.log(res);
-}
+const blog = async () => {
+  const res = await fetch("/blogs");
+  const text = await res.text();
+  const json = JSON.parse(text);
+  pager.callbackMaker(blogHtml)();
+  const container = document.getElementById("container");
+  const commonclass = "flex fd_column w_maxc ps_c p_1rem";
+  const giacomoContainer = new UIDesign({
+    tag: "div",
+    id: "giacomo-container",
+    className: commonclass,
+  });
+  const ariContainer = new UIDesign({
+    tag: "div",
+    id: "ari-container",
+    className: commonclass,
+  });
+  const giacomotitle = new UIDesign({ tag: "h3", id: "giacomo-title" });
+  const arititle = new UIDesign({ tag: "h3", id: "ari-title" });
+  giacomotitle.setInnerText("Giacomo");
+  arititle.setInnerText("Arianna");
+  giacomoContainer.addChild(giacomotitle);
+  ariContainer.addChild(arititle);
+  const createA = (id) => {
+    const a = new UIDesign({ tag: "a", id: id });
+    a.setInnerText(id);
+    a.setHtmlAttribute("href", `./blog/${id}`);
+    return a;
+  };
+  const giacomoBlogs = json.giacomo.map((e) => createA(e));
+  const ariBlogs = json.arianna.map((e) => createA(e));
+  giacomoBlogs.forEach((ui) => giacomoContainer.addChild(ui));
+  ariBlogs.forEach((ui) => ariContainer.addChild(ui));
+  container.appendChild(giacomoContainer.element);
+  container.appendChild(ariContainer.element);
+  container.id = "flex container";
+  container.className = "flex pc_c ta_c w_100vw h_100vh";
+  console.log(giacomoBlogs);
+};
+// async function blog() {
+// }
 
 page("/", index);
 page("/giacomo", giacomo);
@@ -79,10 +120,22 @@ page("/blog", blog);
 page("/blog/:filename", load);
 page();
 
-function load(ctx, next) {
+async function load(ctx, next) {
   console.log(ctx);
-  mdToHtmlString(
-    `assets/${ctx.params.filename}.md`,
-    document.getElementById("content")
-  );
+
+  const container = document.getElementById("container");
+  console.log(container);
+  const res = await fetch(`assets/giacomo/${ctx.params.filename}`);
+  const text = await res.text();
+  container.innerHTML;
+  if (container.firstChild) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+  container.innerHTML = text;
+  // mdToHtmlString(
+  //   `assets/${ctx.params.filename}.md`,
+  //   document.getElementById("content")
+  // );
 }
